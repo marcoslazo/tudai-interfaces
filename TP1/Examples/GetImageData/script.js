4caf45ecb60b3194d2c98b27822124c923345edb
Marcos Lazo <marcos.g.lazo@gmail.com>
2020-09-01 10:32:40 -0300

    Fixed minor problems and code styling

--- TP1/Examples/GetImageData/script.js
+++ TP1/Examples/GetImageData/script.js
@@ -6,34 +6,32 @@ image.crossOrigin = 'Anonymous';
 // El servidor Imgur permite crossOrigin = 'Anonymous'
 //image.src = 'https://i.imgur.com/lmy09aF.jpeg';
 
-// Para cargar imagen de disco, se puede usar la extension Live Server
+// Para cargar imagen de disco, se puede usar la extension Live Server, en gh-pages funciona
 image.src = 'lmy09aF.jpeg';
+
 image.onload = function () {
-    width = this.width;
-    height = this.height;
-
     let context = canvas.getContext('2d');
-    let canvasAspectRatio = (1.0 * canvas.width) / canvas.height;
-    let widthInCanvas = canvas.width
-    let heightInCanvas = canvas.height * canvasAspectRatio
-
     context.fillStyle = "#024359"; // canvas background color
-    context.fillRect(0, 0, width, height);
-    context.drawImage(this, 0, 0, widthInCanvas, heightInCanvas);
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-    let imageData = context.getImageData(0, 0, widthInCanvas, heightInCanvas);
+    let imageAspectRatio = (1.0 * this.height) / this.width;    
+    let imageScaledWidth = canvas.width;
+    let imageScaledHeight = canvas.width * imageAspectRatio;    
+    
+    context.drawImage(this, 0, 0, imageScaledWidth, imageScaledHeight);
 
-    let from = widthInCanvas * 0.3;
-    let to = heightInCanvas * 0.6;
+    let imageData = context.getImageData(0, 0, imageScaledWidth, imageScaledHeight);
 
-    for (let j = from; j < to; j++) {
-        for (let i = from; i < to; i++) {
-            let index = (j + imageData.width * i) * 4;
-            imageData.data[index + 0] = 0;
-            imageData.data[index + 1] = 0;
-            imageData.data[index + 2] = 0;
+    for (let j = 0; j < imageData.height; j++) {
+        for (let i = 0; i < imageData.width; i++) {
+            if (i % 2 == 0) {
+                let index = (i + imageData.width * j) * 4;
+                imageData.data[index + 0] = 0;
+                imageData.data[index + 1] = 0;
+                imageData.data[index + 2] = 0;
+            }
         }
     }
 
-    context.putImageData(imageData, 100, 0);
+    context.putImageData(imageData, 0, 0);
 }
