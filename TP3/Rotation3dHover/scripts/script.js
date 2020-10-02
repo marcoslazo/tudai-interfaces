8c465a7290469a1f196d698d3ede4f026f0bb2b8
Marcos Lazo <marcos.g.lazo@gmail.com>
2020-10-02 09:49:49 -0300

    Minor changes

--- TP3/Rotation3dHover/scripts/script.js
+++ TP3/Rotation3dHover/scripts/script.js
@@ -12,6 +12,6 @@ window.addEventListener("mousemove", (e) => {
 
     const horizontalFactor = maxXAngle * (x / width - 0.5);
     const verticalFactor = -maxYAngle * (y / height - 0.5);
-
-    img.style.transform = `scale(0.8) rotateY(${horizontalFactor}deg) rotateX(${verticalFactor}deg)`;
+    img.style.transformStyle = `preserve-3d`;
+    img.style.transform = `scale(0.8) translate3d(0,0,-100px) rotateY(${horizontalFactor}deg) rotateX(${verticalFactor}deg)`;
 });
